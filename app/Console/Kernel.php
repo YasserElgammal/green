fc37b25c3fdf3c0d73c01afc0ebb971c5ed8d160
Yasser Elgammal <yasser.elgammal@ymail.com>
2026-04-07 01:40:09 +0200

    feat: add initial console kernel and entry point script

---
+++ app/Console/Kernel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Console;
+
+use YasserElgammal\Green\Console\Kernel as CoreKernel;
+
+class Kernel extends CoreKernel
+{
+    protected array $commands = [
+        // App\Console\Commands\MyCommand::class,
+    ];
+}
